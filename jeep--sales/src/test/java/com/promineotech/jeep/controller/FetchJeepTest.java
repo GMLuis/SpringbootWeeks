0ed3662129aa8e76f35f10dd8ac40d83a5902509
lg <lg@localhost>
2021-11-01 18:13:10 -0700

    Week 2 SpringBoot project

--- jeep--sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ jeep--sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -2,12 +2,18 @@ package com.promineotech.jeep.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -28,8 +34,8 @@ import lombok.Getter;
 	    "classpath:flyway/migrations/V1.1__Jeep_Data.sql"},
 	    config = @SqlConfig(encoding = "utf-8"))
 	class FetchJeepTest {
-
-		@Test
+		
+	@Test
 	void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
 		// Given
 		JeepModel model = JeepModel.WRANGLER;
@@ -37,11 +43,15 @@ import lombok.Getter;
 		String uri =
 				String.format("http://localhost:%d/jeeps?model=%s&trim=%s", serverPort, model, trim);
 		// When
-		ResponseEntity<Jeep> response =
-				getRestTemplate().getForEntity(uri, Jeep.class);
-		//ResponseEntity<List<Jeep>> response = restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+		//ResponseEntity<Jeep> response =
+			//	getRestTemplate().getForEntity(uri, Jeep.class);
+		ResponseEntity<List<Jeep>> response = restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
 		//Then
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		
+		List<Jeep> expected = buildExpected();
+		
+		assertThat(response.getBody()).isEqualTo(expected);
 	}
 
 	  @Autowired
@@ -50,5 +60,27 @@ import lombok.Getter;
 
 	  @LocalServerPort
 	  private int serverPort;
+	  
+	  protected List<Jeep> buildExpected(){
+		  List<Jeep> list = new LinkedList<>();
+		  
+		  list.add(Jeep.builder()	  
+			  .modelId(JeepModel.WRANGLER)
+			  .trimLevel("Sport")
+			  .numDoors(2)
+			  .wheelSize(17)
+			  .basePrice(new BigDecimal("28475.00"))
+			  .build());
+		  
+		  list.add(Jeep.builder()	  
+				  .modelId(JeepModel.WRANGLER)
+				  .trimLevel("Sport")
+				  .numDoors(4)
+				  .wheelSize(17)
+				  .basePrice(new BigDecimal("31975.00"))
+				  .build());
+		  
+		  return list;
+	  }
 
 }
